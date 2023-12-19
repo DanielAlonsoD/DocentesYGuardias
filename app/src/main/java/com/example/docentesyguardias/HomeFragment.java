84aa1d913199e01db1ed22f9c6588054017b5d73
DanielAlonsoD <daniel.alodom@sanviatorvalladolid.com>
2023-12-19 11:59:15 +0100

    º_º

--- app/src/main/java/com/example/docentesyguardias/HomeFragment.java
+++ app/src/main/java/com/example/docentesyguardias/HomeFragment.java
@@ -7,58 +7,47 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Bundle usuario = new Bundle();
 
     public HomeFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            usuario = getArguments();
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button botonGestionarPermisos = view.findViewById(R.id.botonGestionarPermisos);
+        Button botonTareasAdministrativas = view.findViewById(R.id.botonTareasAdministrativas);
+        Button botonNotificarAusencias = view.findViewById(R.id.botonNotificarAusencias);
+        Button botonFijarTareas = view.findViewById(R.id.botonFijarTareas);
+        Button botonAsignarGuardias = view.findViewById(R.id.botonAsignarGuardias);
+        Button botonElaboracionInformes = view.findViewById(R.id.botonElaboracionInformes);
+
+        if (usuario.isEmpty()) {
+
+        } else if (usuario.getString("TipoProfesor").equals("Jefe De Estudios")) {
+            botonGestionarPermisos.setVisibility(View.GONE);
+            botonTareasAdministrativas.setVisibility(View.GONE);
+            botonNotificarAusencias.setVisibility(View.GONE);
+        } else {
+            botonFijarTareas.setVisibility(View.GONE);
+            botonAsignarGuardias.setVisibility(View.GONE);
+            botonElaboracionInformes.setVisibility(View.GONE);
+        }
+
+        return view;
     }
 }
