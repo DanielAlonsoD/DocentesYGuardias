caa03627177e7df18f987187e930aa07ae3e02fa
Daniel <daniel.alodom@sanviatorivalladolid.com>
2024-01-10 00:18:41 +0100

    CambiarContrasenaActivity acabado (a falta de agregar el texto de error de los Toast), aparte falta por revisar todos los Toast y convertirlos en Snackbar.

--- app/src/main/java/com/example/docentesyguardias/CambiarContrasenaActivity.java
+++ app/src/main/java/com/example/docentesyguardias/CambiarContrasenaActivity.java
@@ -2,13 +2,63 @@ package com.example.docentesyguardias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class CambiarContrasenaActivity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import Tablas.Profesor;
+
+public class CambiarContrasenaActivity extends AppCompatActivity implements View.OnClickListener {
+    private Bundle usuario;
+    private Profesor profesor;
+    private EditText textoViejaContrasena;
+    private EditText textoNuevaContrasena;
+    private EditText textoConfirmarContrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cambiar_contrasena);
+
+        usuario = getIntent().getExtras();
+        profesor = usuario.getParcelable("profesor");
+
+        MaterialToolbar encabezado = findViewById(R.id.encabezadoCambiarContrasena);
+        textoViejaContrasena = findViewById(R.id.textoInsertarContrasenaCambiarContrasena);
+        textoNuevaContrasena = findViewById(R.id.textoInsertarNuevaContrasenaCambiarContrasena);
+        textoConfirmarContrasena = findViewById(R.id.textoInsertarConfirmarContrasenaCambiarContrasena);
+        FloatingActionButton botonRealizado = findViewById(R.id.botonRealizadoCambiarContrasena);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent actividadPerfil = new Intent(CambiarContrasenaActivity.this, PerfilActivity.class);
+        if (v.getId() == R.id.botonRealizadoCambiarContrasena) {
+            String viejaContrasena = textoViejaContrasena.getText().toString();
+            String nuevaContrasena = textoNuevaContrasena.getText().toString();
+            String confirmarContrasena = textoConfirmarContrasena.getText().toString();
+            if (viejaContrasena.isEmpty() || nuevaContrasena.isEmpty() || confirmarContrasena.isEmpty()) {
+                Toast.makeText(this, R.string.errorTextosVacíos, Toast.LENGTH_SHORT).show();
+            } else if (!profesor.getContrasena().equals(viejaContrasena)) {
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            } else if (nuevaContrasena.equals(confirmarContrasena)) {
+                profesor.setContrasena(nuevaContrasena);
+                usuario.putParcelable("profesor", profesor);
+                actividadPerfil.putExtras(usuario);
+                startActivity(actividadPerfil);
+            } else {
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            actividadPerfil.putExtras(usuario);
+            startActivity(actividadPerfil);
+        }
     }
 }
--- app/src/main/java/com/example/docentesyguardias/InicioSesionActivity.java
+++ app/src/main/java/com/example/docentesyguardias/InicioSesionActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.snackbar.Snackbar;
 
 import Tablas.Profesor;
 
